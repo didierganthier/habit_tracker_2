7fcd75d669ae95cf0f7763bf76f4ae69405d481e
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-13 19:10:34 -0500

    Add shared_preferences and table_calendar dependencies; update main.dart for theme color persistence

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/habit_provider.dart';
 import 'screens/habit_list_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final initialThemeColor = prefs.getInt('themeColor') ?? Colors.blue.value;
   runApp(
     ChangeNotifierProvider(
       create: (context) => HabitProvider(),
-      child: const MyApp(),
+      child: MyApp(initialThemeColor: Color(initialThemeColor)),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  final Color initialThemeColor;
+  const MyApp({super.key, required this.initialThemeColor});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color? _themeColor;
+  @override
+  void initState() {
+    super.initState();
+    _themeColor = widget.initialThemeColor;
+  }
+
+  // Method to update the theme color
+  void setThemeColor(Color color) async {
+    setState(() {
+      _themeColor = color;
+    });
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('themeColor', color.value);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Habit Tracker',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(_themeColor!.value, {
+          50: tintColor(_themeColor!, 0.9),
+          100: tintColor(_themeColor!, 0.7),
+          200: tintColor(_themeColor!, 0.5),
+          300: tintColor(_themeColor!, 0.3),
+          400: tintColor(_themeColor!, 0.1),
+          500: _themeColor!,
+          600: Color(shadeColor(_themeColor!, 0.1)),
+          700: Color(shadeColor(_themeColor!, 0.3)),
+          800: Color(shadeColor(_themeColor!, 0.5)),
+          900: Color(shadeColor(_themeColor!, 0.7)),
+        }),
       ),
-      home: const HabitListScreen(),
+      home: HabitListScreen(
+        setThemeColor: setThemeColor,
+      ),
     );
+  }
+
+  int shadeColor(Color color, double factor) =>
+      (color.value - ((color.value & 0xFFFFFF) * factor)).toInt();
+
+  Color tintColor(Color color, double factor) {
+    final hsl = HSLColor.fromColor(color);
+    final lightness = (1 - factor) + hsl.lightness;
+    return hsl.withLightness(lightness.clamp(0.0, 1.0)).toColor();
   }
 }
--- lib/screens/habit_list_screen.dart
+++ lib/screens/habit_list_screen.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:provider/provider.dart';
 import '../providers/habit_provider.dart';
-import 'habit_detail_screen.dart'; // Import the detail screen
+import 'habit_detail_screen.dart';
 import '../models/habit.dart';
 import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HabitListScreen extends StatefulWidget {
-  const HabitListScreen({super.key});
+  final Function(Color) setThemeColor;
+  const HabitListScreen({super.key, required this.setThemeColor});
 
   @override
   State<HabitListScreen> createState() => _HabitListScreenState();
 }
 
 class _HabitListScreenState extends State<HabitListScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
   @override
   void initState() {
     super.initState();
-    // Load habits when the widget is initialized
     Provider.of<HabitProvider>(context, listen: false).loadHabits();
   }
 
@@ -25,6 +30,14 @@ class _HabitListScreenState extends State<HabitListScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Habit Tracker'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.color_lens),
+            onPressed: () {
+              _showColorPickerDialog(context);
+            },
+          ),
+        ],
       ),
       body: Consumer<HabitProvider>(
         builder: (context, habitProvider, child) {
@@ -34,69 +47,73 @@ class _HabitListScreenState extends State<HabitListScreen> {
             );
           }
 
-          return ListView.builder(
-            itemCount: habitProvider.habits.length,
-            itemBuilder: (context, index) {
-              final habit = habitProvider.habits[index];
-              return Card(
-                margin: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        habit.name,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(habit.description),
-                      const SizedBox(height: 8.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          return Column(children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay; // update `_focusedDay` here as well
+                });
+              },
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: habitProvider.habits.length,
+                itemBuilder: (context, index) {
+                  final habit = habitProvider.habits[index];
+                  return Card(
+                    margin: const EdgeInsets.all(8.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('Frequency: ${habit.frequency} times per week'),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      HabitDetailScreen(habit: habit),
-                                ),
-                              );
-                            },
-                            child: const Text('Edit'),
+                          Text(
+                            habit.name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ],
-                      ),
-                      FutureBuilder<int>(
-                        future: habitProvider.calculateCurrentStreak(habit.id!),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const CircularProgressIndicator();
-                          } else if (snapshot.hasError) {
-                            return Text('Error: ${snapshot.error}');
-                          } else {
-                            final streak = snapshot.data ?? 0;
-                            return Text('Current Streak: $streak days');
-                          }
-                        },
-                      ),
-                      const Divider(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: List.generate(7, (index) {
-                          final day = DateTime.now().subtract(Duration(
-                              days: DateTime.now().weekday - 1 - index));
-                          final formattedDay = DateFormat('E').format(day);
-
-                          return FutureBuilder<bool>(
-                            future: habitProvider.isHabitCompletedToday(
-                                habit.id!, day),
+                          Text(habit.description),
+                          const SizedBox(height: 8.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                  'Frequency: ${habit.frequency} times per week'),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          HabitDetailScreen(habit: habit),
+                                    ),
+                                  );
+                                },
+                                child: const Text('Edit'),
+                              ),
+                            ],
+                          ),
+                          FutureBuilder<int>(
+                            future:
+                                habitProvider.calculateCurrentStreak(habit.id!),
                             builder: (context, snapshot) {
                               if (snapshot.connectionState ==
                                   ConnectionState.waiting) {
@@ -104,33 +121,59 @@ class _HabitListScreenState extends State<HabitListScreen> {
                               } else if (snapshot.hasError) {
                                 return Text('Error: ${snapshot.error}');
                               } else {
-                                final isCompleted = snapshot.data ?? false;
-                                return Column(
-                                  children: [
-                                    Text(formattedDay),
-                                    Checkbox(
-                                      value: isCompleted,
-                                      onChanged: habit.daysOfWeek[index]
-                                          ? (bool? value) {
-                                              habitProvider.toggleCompletion(
-                                                  habit, day);
-                                            }
-                                          : null, // Disable the button
-                                      visualDensity: VisualDensity.compact,
-                                    ),
-                                  ],
-                                );
+                                final streak = snapshot.data ?? 0;
+                                return Text('Current Streak: $streak days');
                               }
                             },
-                          );
-                        }),
+                          ),
+                          const Divider(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: List.generate(7, (index) {
+                              final day = DateTime.now().subtract(Duration(
+                                  days: DateTime.now().weekday - 1 - index));
+                              final formattedDay = DateFormat('E').format(day);
+
+                              return FutureBuilder<bool>(
+                                future: habitProvider.isHabitCompletedToday(
+                                    habit.id!, day),
+                                builder: (context, snapshot) {
+                                  if (snapshot.connectionState ==
+                                      ConnectionState.waiting) {
+                                    return const CircularProgressIndicator();
+                                  } else if (snapshot.hasError) {
+                                    return Text('Error: ${snapshot.error}');
+                                  } else {
+                                    final isCompleted = snapshot.data ?? false;
+                                    return Column(
+                                      children: [
+                                        Text(formattedDay),
+                                        Checkbox(
+                                          value: isCompleted,
+                                          onChanged: habit.daysOfWeek[index]
+                                              ? (bool? value) {
+                                                  habitProvider
+                                                      .toggleCompletion(
+                                                          habit, day);
+                                                }
+                                              : null, // Disable the button
+                                          visualDensity: VisualDensity.compact,
+                                        ),
+                                      ],
+                                    );
+                                  }
+                                },
+                              );
+                            }),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
+                    ),
+                  );
+                },
+              ),
+            ),
+          ]);
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -159,6 +202,33 @@ class _HabitListScreenState extends State<HabitListScreen> {
         },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  Future<void> _showColorPickerDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pick a theme color!'),
+          content: SingleChildScrollView(
+            child: BlockPicker(
+              pickerColor: Theme.of(context).primaryColor,
+              onColorChanged: (color) {
+                widget.setThemeColor(color);
+              },
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Got it!'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
